e8616a9c2caf91e784a7360dd7b3276fbb3ba0c6
Crosby <crosby.winfield@outlook.com>
2023-01-24 16:47:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src",randomDiceImage1)
+document.querySelector(".img2").setAttribute("src",randomDiceImage2)
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player One Wins!"
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player Two Wins!🚩"
+} else if (randomNumber1 == randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!"
+}
